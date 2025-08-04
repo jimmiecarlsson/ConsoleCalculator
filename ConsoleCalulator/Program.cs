49e5e276f6bd762e7757d06c9eecab0627411d4c
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-04 18:16:41 +0200

    Lade till Substraktion

--- ConsoleCalulator/Program.cs
+++ ConsoleCalulator/Program.cs
@@ -1,18 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 bool appRullar = true;
-double tal1 = 0, tal2 = 0;
+
 
 while (appRullar)
 {
     Console.Clear();
-    Console.WriteLine("Meny:");
+    Console.WriteLine("Välkommen till min konsol-kalkylator!\n");
+    Console.WriteLine("Meny:\n");
     Console.WriteLine("1. Addition");
     Console.WriteLine("2. Subtraktion");
     Console.WriteLine("3. Multiplikation");
     Console.WriteLine("4. Division");
-    Console.WriteLine("5. Avsluta");
-    Console.WriteLine("Gör ditt val: ");
+    Console.WriteLine("5. Avsluta\n");
+    Console.WriteLine("Gör ditt val:");
 
     ConsoleKeyInfo menyVal = Console.ReadKey();
 
@@ -22,17 +23,28 @@ while (appRullar)
     {
         case '1':
             Console.Clear();
-            Console.WriteLine("\n\nDu valde Addition.");
-            Console.WriteLine($"Summan är {getAddition(tal1, tal2)}");
+            Console.WriteLine("Du valde Addition.");
+            Console.WriteLine("Skriv in två tal som du vill addera.\n");
+
+            double summa = getAddition();
+            Console.WriteLine($"Summan är {summa}");
+
             Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
             Console.ReadKey();
             break;
+
         case '2':
             Console.Clear();
             Console.WriteLine("\n\nDu valde Subtraktion.");
+            Console.WriteLine("Skriv in två tal som du vill subtrahera.\n");
+
+            double differens = getSubstraktion();
+            Console.WriteLine($"Differensen är {differens}");
+
             Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
             Console.ReadKey();
             break;
+
         case '3':
             Console.Clear();
             Console.WriteLine("\nDu valde Multiplikation.");
@@ -58,12 +70,57 @@ while (appRullar)
 
     }
 
-    double getAddition(double tal1, double tal2){
-        Console.WriteLine("Skriv in tal 1:");
-        tal1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Skriv in tal 2:");
-        tal2 = Convert.ToDouble(Console.ReadLine());
-        return (tal1 + tal2);
+
+    // Metod för att hämta och addera två tal
+
+    double getAddition()
+    {
+        double tal1 = 0, tal2 = 0;
+
+        Console.WriteLine("Skriv in tal 1 + <RETURN>:");
+        // Mata in och validera det första numret
+        while (!double.TryParse(Console.ReadLine(), out tal1))
+            {
+                Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+            }
+
+        Console.WriteLine("Skriv in tal 2 + <RETURN>:");
+        // Mata in och validera det andra numret
+        while (!double.TryParse(Console.ReadLine(), out tal2))
+        {
+            Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+        }
+
+        // Summera, avrunda och returnera summan
+        double calcSumma = (tal1 + tal2);
+        calcSumma = Math.Round(calcSumma, 2);
+
+        return (calcSumma);
+    }
+
+    double getSubstraktion()
+    {
+        double tal1 = 0, tal2 = 0;
+
+        Console.WriteLine("Skriv in tal 1 + <RETURN>:");
+        // Mata in och validera det första numret
+        while (!double.TryParse(Console.ReadLine(), out tal1))
+        {
+            Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+        }
+
+        Console.WriteLine("Skriv in tal 2 + <RETURN>:");
+        // Mata in och validera det andra numret
+        while (!double.TryParse(Console.ReadLine(), out tal2))
+        {
+            Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+        }
+
+        // Summera, avrunda och returnera summan
+        double calcSumma = (tal1 - tal2);
+        calcSumma = Math.Round(calcSumma, 2);
+
+        return (calcSumma);
     }
 
 
