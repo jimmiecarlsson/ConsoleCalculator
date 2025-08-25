1e837f3f824eac5d5e97a5f57efc13610e83e0b7
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-25 17:01:17 +0200

    Add 2 tests for the Dividera.cs

---
+++ xUnitTest/DivideraTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using ConsoleCalulator.Actions;
+
+namespace xUnitTest
+{
+    public class DivideraTests
+    {
+
+        [Fact]
+        public void Dividera_TvaPositivaTal_ReturnerarKvot()
+        {
+            // Arrange
+            Dividera dividera = new Dividera();
+
+            double a = 10;
+            double b = 4;
+
+            // Act
+            double result = dividera.Divi(a, b);
+
+            // Assert
+            Assert.Equal(2.50, result, precision: 2);
+        }
+
+        [Fact]
+        public void Dividera_TäljarenNoll_ReturnerarNoll()
+        {
+            // Arrange
+            Dividera dividera = new Dividera();
+
+            double a = 0;
+            double b = 5;
+
+            // Act
+
+            double result = dividera.Divi(a,b);
+
+            // Assert
+            Assert.Equal(0.00, result, precision: 2);
+
+        }
+
+
+
+    }
+}
