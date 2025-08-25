586ba01fc13fe79b478c4c2696513975ad5623bf
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-25 13:42:19 +0200

    Add more unit tests

--- xUnitTest/AdderaTests.cs
+++ xUnitTest/AdderaTests.cs
@@ -20,5 +20,42 @@ namespace xUnitTest
             Assert.Equal(5.68, result, precision: 2);
         }
 
+        [Fact]
+        public void Add_PositivtOchNegativtNummer_ReturneraRoundedSum()
+        {
+            // Arrange
+            Addera adderaMera = new Addera();
+
+            double a = -6.8;
+            double b = 2.345678;
+
+            // Act
+            double result = adderaMera.Add(a, b);
+            double result2 = adderaMera.Add(-3.5456, 0);
+
+            // Assert
+
+            Assert.Equal(-4.45, result, precision: 2);
+            Assert.Equal(-3.55, result2, precision: 2);
+        }
+
+        [Fact]
+        public void Add_TvaFlyttalForAvrundningstest_ReturneraRoundedSum()
+        {
+            // Arrange
+            Addera addera = new Addera();
+
+            double a = 0.1;
+            double b = 0.2;
+
+            // Act
+
+            double result = addera.Add(a, b);
+            double result2 = addera.Add(0.22, a);
+
+            // Assert
+            Assert.Equal(0.30, result, precision: 2);
+            Assert.Equal(0.32, result2, precision: 2);
+        }
     }
 }
