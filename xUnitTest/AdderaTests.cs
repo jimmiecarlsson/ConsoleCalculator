a082db4fdee2213a7d394ee45e310a9460bdb936
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-25 12:50:39 +0200

    Run the first test Add a project reference

---
+++ xUnitTest/AdderaTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using ConsoleCalulator.Actions;
+
+namespace xUnitTest
+{
+    public class AdderaTests
+    {
+
+        [Fact]
+        public void Add_TvaPositiveNummer_ReturneraRoundedSum()
+        {
+            // Arrange
+            var addera = new Addera();
+            double a = 2.345, b = 3.335;
+
+            // Act
+            double result = addera.Add(a, b);
+
+            // Assert
+            Assert.Equal(5.68, result, precision: 2);
+        }
+
+    }
+}
