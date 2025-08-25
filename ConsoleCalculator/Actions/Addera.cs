de7607f346722992391658893a7eb2851551b7b8
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-25 12:26:39 +0200

    Refactor the code for testing

--- ConsoleCalculator/Actions/Addera.cs
+++ ConsoleCalculator/Actions/Addera.cs
@@ -4,30 +4,50 @@ namespace ConsoleCalulator.Actions
 {
     public class Addera
     {
-
+        public double Add(double a, double b)
+        {
+            return Math.Round(a + b, 2);
+        }
         public double GetAddition()
         {
-            double tal1 = 0, tal2 = 0;
+            //Bort refaktorerade kod för att kunna göra enhetstester med xUnit
+            //Skapade metoder för att läsa in double
 
-            Console.WriteLine("Skriv in tal 1 + <RETURN>:");
-            // Mata in och validera det första numret
-            while (!double.TryParse(Console.ReadLine(), out tal1))
+            //Console.WriteLine("Skriv in tal 1 + <RETURN>:");
+            //// Mata in och validera det första numret
+            //while (!double.TryParse(Console.ReadLine(), out tal1))
+            //{
+            //    Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+            //}
+
+            //Console.WriteLine("Skriv in tal 2 + <RETURN>:");
+            //// Mata in och validera det andra numret
+            //while (!double.TryParse(Console.ReadLine(), out tal2))
+            //{
+            //    Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
+            //}
+
+            //// Summera, avrunda och returnera summan
+            //double calcSumma = tal1 + tal2;
+            //calcSumma = Math.Round(calcSumma, 2);
+
+            double tal1 = ReadDouble("Skriv in tal 1 + <RETURN>:");
+            double tal2 = ReadDouble("Skriv in tal 2 + <RETURN>:");
+
+            return Add(tal1, tal2);
+        }
+
+        private static double ReadDouble(string prompt)
+        {
+            Console.WriteLine(prompt);
+
+            double value;
+
+            while (!double.TryParse(Console.ReadLine(), out value))
             {
                 Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
             }
-
-            Console.WriteLine("Skriv in tal 2 + <RETURN>:");
-            // Mata in och validera det andra numret
-            while (!double.TryParse(Console.ReadLine(), out tal2))
-            {
-                Console.WriteLine("Tänka på att använda komma-tecken för decimaler, försök igen:");
-            }
-
-            // Summera, avrunda och returnera summan
-            double calcSumma = tal1 + tal2;
-            calcSumma = Math.Round(calcSumma, 2);
-
-            return calcSumma;
+            return value;
         }
 
 
